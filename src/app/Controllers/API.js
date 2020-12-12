6b2a179d87e0dee301711f921cbc639dd2790b97
lucas-felinto <lucasfepinto@gmail.com>
2020-12-12 13:30:43 -0300

    refactored APIController and add following request

--- src/app/Controllers/API.js
+++ src/app/Controllers/API.js
@@ -21,21 +21,58 @@ module.exports = {
         return res.status(404).json('User not found');
       }
 
-      const followersNumber = userApi.data.followers;
-      const followersPerPage = 100;
-      const pages = Math.round(followersNumber/followersPerPage);
-
-      const followersArray = [];
-
-      for (let i = 1; i <= pages; i++) {
-        const followersByPage = await axios.get(`https://api.github.com/users/${user}/followers?per_page=${followersPerPage}&page=${i}`);
-
-        followersArray.push(followersByPage.data);
+      let params = {
+        usersNumber: userApi.data.followers,
+        usersPerPage: 100,
+        user: user,
+        usersStatus: 'followers'
       }
 
-      return res.status(200).json(followersArray);
+      const followers = await this.getUsers(params);
+
+      params = {
+        usersNumber: userApi.data.following,
+        usersPerPage: 100,
+        user: user,
+        usersStatus: 'following'
+      }
+
+      const following = this.getUsers(params);
+
+      // if (followers.length > 1) {
+      //   for (let array of followers) {
+      //     // novo for verificando com quem está seguindo quem te segue ou não ou um filter
+      //     console.log(array)
+      //   }
+      // } else {
+      //   // fazer um for apenas verificando com quem está seguindo
+      //   followers.push(followersArray[0]);
+      // }
+
+      return res.status(200).json(following);
     } catch (e) {
+      console.log(e)
       Log.error(`APIController - getApi Endpoint - ${e.message}`);
+      return res.status(500).json(e.message);
+    }
+  },
+
+  async getUsers(params) {
+    try {
+      console.log(params)
+      const users = [];
+      const pages = Math.round(params.usersNumber/params.usersPerPage);
+
+      for (let i = 1; i <= pages; i++) {
+        const followersByPage = await axios.get(`https://api.github.com/users/${params.user}/${params.usersStatus}?per_page=${params.usersPerPage}&page=${i}`);
+
+        users.push(followersByPage.data);
+      }
+
+      return users;  
+    } catch (e) {
+      console.log(e)
+      Log.error(`APIController - getUsers Endpoint - ${e.message}`);
       return res.status(500).json(e.message);
     }
   }
