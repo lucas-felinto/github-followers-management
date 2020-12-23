434f1121ce71a1907668a9c173b244b2abbfd82c
lucas-felinto <lucasfepinto@gmail.com>
2020-12-23 13:24:15 -0300

    feat: add middleware and endpoints to less than 100 users

--- src/app/Controllers/API.js
+++ src/app/Controllers/API.js
@@ -2,67 +2,14 @@ const axios = require('axios');
 const Log = require('../../utils/logger');
 
 module.exports = {
-  async getApi(req, res) {
+  async getUsersNotFollowYouBack(req, res) {
     try {
-      const user = req.query.user;
-
-      if (!user) {
-        return res.status(400).json('User is required');
-      }
-
-      const gitHubApi = `https://api.github.com/users/${user}`;
-      const userApi = await axios.get(gitHubApi);
-
-      if (userApi.status === 403) {
-        return res.status(403).json('You have reached the request limit');
-      }
-
-      if (!userApi) {
-        return res.status(404).json('User not found');
-      }
-
-      const getUsers = async function(params) {
-        try {
-          const pages = Math.round(params.usersNumber/params.usersPerPage);
-          const users = [];
-    
-          for (let i = 1; i <= pages; i++) {
-            const followersByPage = await axios.get(`https://api.github.com/users/${params.user}/${params.usersStatus}?per_page=${params.usersPerPage}&page=${i}`);
-    
-            users.push(followersByPage.data);
-          }
-          
-          return users;  
-        } catch (e) {
-          Log.error(`APIController - getUsers Function - ${e.message}`);
-          return json('Cannot get users:', e.message);
-        }
-      }
-
-      let params = {
-        usersNumber: userApi.data.followers,
-        usersPerPage: 100,
-        user: user,
-        usersStatus: 'followers'
-      }
-
-      const followers = await getUsers(params);
-
-      params = {
-        usersNumber: userApi.data.following,
-        usersPerPage: 100,
-        user: user,
-        usersStatus: 'following'
-      }
-
-      const following = await getUsers(params);
-
-      
+      const apiData = req.apiData;
 
       const usersNotFollowYouBack = [];
 
-      for (let followingUser of following[0]) {
-        let FollowYou = followers[0].find(user => 
+      for (let followingUser of apiData.following[0]) {
+        let FollowYou = apiData.followers[0].find(user => 
           user.login === followingUser.login
         );
 
@@ -74,7 +21,30 @@ module.exports = {
       return res.status(200).json(usersNotFollowYouBack);
     } catch (e) {
       console.log(e)
-      Log.error(`APIController - getApi Endpoint - ${e.message}`);
+      Log.error(`APIController - getUsersNotFollowYouBack Endpoint - ${e.message}`);
+      return res.status(500).json(e.message);
+    }
+  },
+
+  async getUsersYouNotFollowBack(req, res) {
+    try {
+      const apiData = req.apiData;
+
+      const usersYoutNotFollowBack = [];
+
+      for (let followUser of apiData.followers[0]) {
+        let FollowYou = apiData.following[0].find(user => 
+          user.login === followUser.login
+        );
+
+        if (!FollowYou) {
+          usersYoutNotFollowBack.push(followUser);
+        }
+      }
+
+      return res.status(200).json(usersYoutNotFollowBack);
+    } catch (e) {
+      Log.error(`APIController - getUsersYouNotFollowBack Endpoint - ${e.message}`);
       return res.status(500).json(e.message);
     }
   }
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const APIController = require('./app/Controllers/API')
+const APIController = require('./app/Controllers/API');
+const APIMiddleware = require('./app/Middlewares/API');
 
 const Routes = express.Router();
 
-Routes.get('/get-api', APIController.getApi);
+Routes.get(
+  '/not-follow-you', 
+  APIMiddleware.getApi, 
+  APIController.getUsersNotFollowYouBack
+  );
+
+Routes.get(
+  '/you-not-follow', 
+  APIMiddleware.getApi, 
+  APIController.getUsersYouNotFollowBack
+  );
 
 module.exports = Routes;
