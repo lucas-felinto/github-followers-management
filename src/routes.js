e2c2f8d9c71fb3a6f4f6c347eae91136c6db0fec
lucas-felinto <lucasfepinto@gmail.com>
2021-01-21 16:20:01 -0300

    feat: include welcome route

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const APIMiddleware = require('./api/Middlewares/APIMiddleware');
 const Routes = express.Router();
 
 Routes.get('/', (req, res) => {
-  const response = 'To discover users you follow but you not follow back use the /not-follow-you?user=your-github-user. To discover users who you follow but not follow you back use /you-not-follow?user=your-github-user. For any questions access: https://github.com/lucas-felinto/github-followers-management or https://github-followers-management.herokuapp.com/api/v1/documentation/';
+  const response = `Welcome to GitHub Followers Management API. To discover users you follow but you not follow back use the /not-follow-you?user=your-github-user. To discover users who you follow but not follow you back use /you-not-follow?user=your-github-user. For any questions access: https://github.com/lucas-felinto/github-followers-management or https://github-followers-management.herokuapp.com/api/v1/documentation/`;
   
   return res.status(200).json(response);
 })
